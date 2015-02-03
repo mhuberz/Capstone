17a4eeb505de05d98d4e242569fc9666b05ac254
mhuberz <matthew.a.hubers@gmail.com>
2015-02-03 15:24:31 -0500

    First Commit

---
+++ InveRsePG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	private Vector2 pos;
+	private bool moving = false;
+
+
+	void Start () 
+	{
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckInput();
+
+		if(moving)
+		{
+
+			transform.position = pos;
+			moving = false;
+		}
+	}
+
+	private void CheckInput()
+	{
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			pos += Vector2.right;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.A))
+		{
+			pos -= Vector2.right;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.W))
+		{
+			pos += Vector2.up;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.S))
+		{
+			pos -= Vector2.up;
+		}
+		moving = true;
+	}
+}
