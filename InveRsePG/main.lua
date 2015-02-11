1ea008c6d944c1640a3e6fa5ea8383833d1b7b4e
mhuberz <matthew.a.hubers@gmail.com>
2015-02-11 14:56:27 -0500

    fixed camera and added player.lua

--- InveRsePG/main.lua
+++ InveRsePG/main.lua
@@ -1,39 +1,41 @@
 require('camera')
+require('player')
+--[[todo: 	character sprite
+			map
+			enemies
+			battle system
+	]]
+
 
 function love.load()
-	player = {
-		grid_x = 256,
-		grid_y = 256,
-		act_x = 200,
-		act_y = 200,
-		speed = 10
-	}
+	camera:setPosition(player.grid_x- love.window.getWidth()/2+32,player.grid_y- love.window.getHeight()/2+32)
 end
 
 function love.update(dt)
 	player.act_y = player.act_y - ((player.act_y - 
 		player.grid_y) * player.speed * dt)
 	player.act_x = player.act_x - ((player.act_x - 
-		player.grid_x) * player.speed * dt)
-	
+		player.grid_x) * player.speed * dt)	
 end
 
 function love.draw()
 	camera:set()
 	love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
-	love.graphics.print(player.act_x,200)
-	love.graphics.print(player.act_y,200,100)
 	camera:unset()
 end
 
 function love.keypressed(key)
 	if key == "w" then 
-		player.grid_y = player.grid_y - 32 
+		player.grid_y = player.grid_y - 32
+		camera:move(0,-32) 
 	elseif key == "s" then
 		player.grid_y = player.grid_y + 32
+		camera:move(0,32)
 	elseif key == "a" then 
 		player.grid_x = player.grid_x - 32
+		camera:move(-32,0)
     elseif key == "d" then 
     	player.grid_x = player.grid_x + 32
+    	camera:move(32,0)
     end 
 end
---
+++ InveRsePG/player.lua
@@ -0,0 +1,6 @@
+player = {}
+player.grid_x = 256
+player.grid_y = 256
+player.act_x = 200
+player.act_y = 200
+player.speed = 7
